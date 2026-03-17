6cc928610ce0f4182195b89d7d9b14822a3995ac
ninjadrax <112233848+NinjaDrax@users.noreply.github.com>
2026-03-17 12:07:42 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,21 +5,9 @@ const cors = require('cors');
 
 const app = express();
 
-const allowedOrigins = [
-  'http://localhost:3000',
-  'http://localhost:5173',
-  process.env.FRONTEND_URL
-].filter(Boolean);
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.includes(origin)) return callback(null, true);
-    return callback(new Error('Not allowed by CORS'));
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  origin: true,
+  credentials: true
 }));
 
 app.options('*', cors());
